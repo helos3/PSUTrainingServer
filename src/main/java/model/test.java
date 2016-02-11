40bbf6d6c9b240c59cf28f3bd0f841e34c102ad9
Rushan <446735r@gmail.com>
2016-02-11 12:39:36 +0300

    adding model and test requests

---
+++ src/main/java/model/test.java
@@ -0,0 +1,47 @@
+package model;
+
+import org.json.simple.JSONObject;
+
+import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import static spark.Spark.*;
+
+public class test {
+    static Connection connection = null;
+
+    public static void main(String[] args) {
+        try {
+            DriverManager.registerDriver(new com.mysql.jdbc.Driver());
+            connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/?user=root&password=rushan196654rZ");
+        } catch (Exception e) {
+        }
+        get("/hello", (req, res) -> "Hello World");
+        get("/get_ranks", (req, res) -> {
+
+            Statement stmt = null;
+            ResultSet rs = null;
+
+            JSONObject resultJson = new JSONObject();
+
+            stmt = connection.createStatement();
+            rs = stmt.executeQuery("SELECT `academic_degree`.`id`,\n" +
+                    "    `academic_degree`.`name`\n" +
+                    "FROM `mydb`.`academic_degree`;\n");
+
+            if (stmt.execute("SELECT `academic_degree`.`id`,\n" +
+                    "    `academic_degree`.`name`\n" +
+                    "FROM `mydb`.`academic_degree`;\n")) {
+                rs = stmt.getResultSet();
+            }
+            while (rs.next()) {
+                // Напечатать значения в текущей строке.
+                resultJson.put(rs.getInt("id"), rs.getString("name"));
+            }
+            return resultJson;
+        });
+
+    }
+
+}
